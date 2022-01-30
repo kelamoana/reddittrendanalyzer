c1eb69ecba3fac56c959a957a0f0af7e9f576273
kelamoana <kela.moana@gmail.com>
2022-01-29 17:05:57 -0800

    Added Base Code
    Added some files for organization as well as moved the data to a file

---
+++ helpers.py
@@ -0,0 +1,4 @@
+# CS 175 Winter 2022 - Reddit Trend Analyzer
+# Cullen P.P. Moana
+# Sushmasri Katakam
+# Ethan H. Nguyen
---
+++ main.py
@@ -0,0 +1,8 @@
+# CS 175 Winter 2022 - Reddit Trend Analyzer
+# Cullen P.P. Moana
+# Sushmasri Katakam
+# Ethan H. Nguyen
+# The following module will be to run the software
+
+
+print("testing.... Hello World!")
