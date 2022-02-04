3932f374bc6f54dead07fe2d21a6cb403b84ee15
kelamoana <kela.moana@gmail.com>
2022-02-04 11:12:13 -0800

    Finalized Preprocessing Function
    Updated Preprocess function so it works on full TSV files.

--- helpers.py
+++ helpers.py
@@ -22,24 +22,30 @@ def preprocess_data_tsv(filename = "data/train.tsv"):
     Returns: A dict where each val is a tuple containing 
     the text and a list of its sentiments.
     """
+    
+    # Create File Obj
     file_obj = open(filename, "r", encoding='utf-8', errors='ignore')
 
+    # Dict to return
     file_lines = {}
 
-    for i in range(10):
+    # Grab the first line
+    line = file_obj.readline()
 
-        line = file_obj.readline()
+    # While we haven't reached the end
+    while line != '':
+
+        # Create list of [text, sentiments, id] and remove newlines
         line_to_append = line.replace("\n", '').split('\t')
-        line_to_append[1] = [int(i) for i in line_to_append[1].replace(' ', '').split(',')]
-        
-        # FOR DEBUG
-        # print("Text: ", line_to_append[0])
-        # print("Emotions: ", end='')
-        # for emotion in line_to_append[1]:   
-        #     print(emotions[emotion], end=' ')
-        # print() # Print a newline
 
+        # Turn sentiments from str to list of ints
+        line_to_append[1] = [int(i) for i in line_to_append[1].replace(' ', '').split(',')]
+
+        # Add to dict
         file_lines[line_to_append[2]] = (line_to_append[0], line_to_append[1])
+
+        # Grab next line
+        line = file_obj.readline()
     
     return file_lines
 
@@ -49,4 +55,4 @@ def convert_to_wordemb(prerpocessed_data):
 
 if __name__ == "__main__":
 
-    print(preprocess_data_tsv())
+    preprocess_data_tsv()
