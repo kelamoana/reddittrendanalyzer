704694b68e6fc8b94ac890d1dd58ccc7a531d2df
kelamoana <kela.moana@gmail.com>
2022-02-07 20:37:25 -0800

    Created a Function to combine sentences with classes from text files

--- helpers.py
+++ helpers.py
@@ -62,6 +62,40 @@ def preprocess_data_tsv(filename = "data/train.tsv"):
 
     return file_lines
 
+def get_sentences_and_classes(corpus = "data/logisticRegression/XTrainData.txt", classes = "data/logisticRegression/YTrainData.txt"):
+    """
+    A function to process all of the training data and turn it
+    into a usable data structure.
+
+    Parameters: corpus - A txt file, classes - a txt file
+    Returns: A dict where each val is a tuple containing
+    the text and a list of its sentiments.
+    """
+
+    # Create File Obj
+    file_obj1 = open(corpus, "r", encoding='utf-8', errors='ignore')
+    file_obj2 = open(classes, "r", encoding='utf-8', errors='ignore')
+
+    # Dict to return
+    file_lines = {}
+
+    # Grab the first line
+    line1 = file_obj1.readline()
+    line2 = file_obj2.readline()
+
+    # While we haven't reached the end
+    while line1 != '':
+
+        # Add to dict
+        file_lines[line1] = line2
+
+        # Grab next line
+        line1 = file_obj1.readline()
+        line2 = file_obj2.readline()
+
+    return file_lines
+
+
 def filter_stopwords(sentence):
     """
     Removes stop words from a sentence.
@@ -81,7 +115,8 @@ def convert_to_wordemb(prepocessed_data):
     Converts preprocessed data into word embeddings
 
     Parameters: preprocessed_data - a dict of sentences
-    Returns: A dict, where each sentence is now a list of vectors representing each word.
+    Returns: A dict, where each sentence is a key and associated with
+        a list of vectors representing each word.
     """
     
     # Initialize Word2Vec model
@@ -96,15 +131,28 @@ def convert_to_wordemb(prepocessed_data):
     # FOR DEBUG
     # print([i for i in model.wv.index_to_key])
 
-    embedded_word_list = list()
+    embedded_word_list = dict()
 
     for s in prepocessed_data:
         
-        embedded_word_list.append([model.wv[word] for word in s if word in model.wv])
+        embedded_word_list[s] = [model.wv[word] for word in s if word in model.wv]
 
     return embedded_word_list
 
 if __name__ == "__main__":
 
-    data_dict = preprocess_data_tsv()
-    word_embed_list = convert_to_wordemb([text for text, sent in data_dict.values()])
+    # data_dict = preprocess_data_tsv()
+    # word_embed_list = convert_to_wordemb([text for text, sent in data_dict.values()])
+    
+    sent_and_classes = get_sentences_and_classes()
+    word_embed_dict = convert_to_wordemb([sent for sent in sent_and_classes])
+
+    i = 0
+    for s in sent_and_classes:
+        print(f"Sentence: {s} -------> Class: {sent_and_classes[s]}")
+
+        if i == 1:
+            break
+
+        i+=1
+
